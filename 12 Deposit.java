b3fcfc75dc5b7a976417b43812c179be11340b8b
Nowrin Ibnat <nowrinima890@gmail.com>
2022-05-10 00:50:09 +0600

    atm-study-case

---
+++ 12 Deposit.java
@@ -0,0 +1,54 @@
+package atmachine;
+
+public class Deposit extends Transaction {
+    private double amount;
+    private Keypad keypad;
+    private DepositSlot depositSlot;
+    private final static int CANCELED = 0;
+
+    public Deposit(int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase, Keypad atmKeypad,
+            DepositSlot atmDepositSlot) {
+        super(userAccountNumber, atmScreen, atmBankDatabase);
+        keypad = atmKeypad;
+        depositSlot = atmDepositSlot;
+    }
+
+    @Override
+    public void execute() {
+        BankDatabase bankDatabase = getBankDatabase();
+        Screen screen = getScreen();
+
+        amount = promptForDepositAmount();
+        if (amount != CANCELED) {
+            screen.displayMsg("\nPlease insert a deposit envelope containing ");
+            screen.displayDollarAmount(amount);
+            screen.displayMsgLine(".");
+
+            boolean envelopeReceived = depositSlot.isEnvelopeReceived();
+            if (envelopeReceived) {
+                screen.displayMsgLine("\nYour envelope has been " +
+                        "received.\nNOTE: The money just deposited will not " +
+                        "be available until we verify the amount of any " +
+                        "enclosed cash and your checks clear.");
+                bankDatabase.credit(getAccountNumber(), amount);
+            } else {
+                screen.displayMsgLine("\nYou did not insert an " +
+                        "envelope, so the ATM has canceled your transaction.");
+            }
+        } else {
+            screen.displayMsgLine("\nCanceling the transaction...");
+        }
+    }
+
+    private double promptForDepositAmount() {
+        Screen screen = getScreen();
+
+        screen.displayMsg("Enter deposits in CENTS (or 0 to cancel):");
+        int input = keypad.getInput();
+        if (input == CANCELED)
+            return CANCELED;
+        else {
+            return (double) input / 100;
+        }
+    }
+}
